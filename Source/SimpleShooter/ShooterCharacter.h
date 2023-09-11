57141b78aaef2db1a2d698fe71b3547ea270799e
mrsagcan <mrsagcan@yahoo.com>
2023-09-11 13:50:50 +0300

    Blend Space for Character Movement

--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -31,4 +31,9 @@ private:
 
 	void MoveForward(float AxisValue);
 	void MoveRight(float AxisValue);
+	void LookUpRate(float AxisValue);
+	void LookRightRate(float AxisValue);
+
+	UPROPERTY(EditAnywhere)
+		float RotationRate = 10;
 };
